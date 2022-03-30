92ee6b1ad8b28f3760a6423801f9a51151d62485
Sara <tahsa067@student.otago.ac.nz>
2022-03-30 23:51:23 +1300

    unchanged the last commit

--- phase1-starting-projects-master/accounts-service/src/main/java/graphql/AccountFetcher.java
+++ phase1-starting-projects-master/accounts-service/src/main/java/graphql/AccountFetcher.java
@@ -9,8 +9,6 @@ import static io.jooby.Reified.map;
 import java.math.BigDecimal;
 import java.util.Map;
 
-
-
 public class AccountFetcher {
 
     private final AccountserviceDAO dao;
@@ -25,32 +23,33 @@ public class AccountFetcher {
             return dao.getCustomerById(id);
         };
     }
+
     public DataFetcher getCreateCustomerFetcher() {
-    return (DataFetchingEnvironment dfe) -> {
-        Map map = dfe.getArgument("customer");
-        ObjectMapper mapper = new ObjectMapper();
-        Customer newCustomer = mapper.convertValue(map, Customer.class);
-        return dao.addCustomer(newCustomer);
-    };
-}
+        return (DataFetchingEnvironment dfe) -> {
+            Map map = dfe.getArgument("customer");
+            ObjectMapper mapper = new ObjectMapper();
+            Customer newCustomer = mapper.convertValue(map, Customer.class);
+            return dao.addCustomer(newCustomer);
+        };
+    }
 
     public DataFetcher getChangeGroupFetcher() {
         return (DataFetchingEnvironment dfe) -> {
-             String id = dfe.getArgument("id");
-            Customer groups = dfe.getArgument("groups");
-         //ObjectMapper mapper = new ObjectMapper();
-        //Customer groups = mapper.convertValue(group, Customer.class);
+            String id = dfe.getArgument("id");
+            String group = dfe.getArgument("group");
+            ObjectMapper mapper = new ObjectMapper();
+            Customer groups = mapper.convertValue(group, Customer.class);
             return dao.changeGroup(id, groups);
-       };
+        };
     }
-
 
     public DataFetcher getCustomerFetcher() {
         return (DataFetchingEnvironment dfe) -> {
-           return dao.getCustomer();
-       };
+            return dao.getCustomer();
+        };
     }
 //
+
     public DataFetcher getDeleteCustomerFetcher() {
         return (DataFetchingEnvironment dfe) -> {
             String id = dfe.getArgument("id");
@@ -59,5 +58,3 @@ public class AccountFetcher {
     }
 
 }
-
-
